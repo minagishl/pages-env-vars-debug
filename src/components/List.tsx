a467a0a8185588e65e81bd8a0ce26a479ae70521
minagishl <mvxd568.co@icloud.com>
2024-02-20 01:30:36 +0900

    Add runtime

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,7 +3,11 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
+export const runtime = 'edge';
+
 export const List = () => {
+	const currentDate = new Date(); // Get current date
+
 	const envTable = Object.keys(process.env)
 		.map((key) => {
 			return `| ${key} | ${process.env[key]} |`;
@@ -11,8 +15,9 @@ export const List = () => {
 		.join('\n');
 
 	const markdownTable =
-		'| VAR                  | Value                |\n' +
-		'| -------------------- | -------------------- |\n' +
+		'| VAR                                | Value                |\n' +
+		'| ---------------------------------- | -------------------- |\n' +
+		`| Date (Not an environment variable) | ${currentDate}       |\n` + // Add current date to the table
 		envTable;
 
 	// debug
